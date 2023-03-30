48ffbbd43750b6315d99d4eb559b58e7222018f7
Thiago Furlan <thiago.sfurlan@outlook.com>
2023-03-30 14:18:58 -0300

    fix attribute

--- src/config/scraper.js
+++ src/config/scraper.js
@@ -1,25 +1,27 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const logger = require('./logger');
-const { parse } = require('../utils/number');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const logger = require("./logger");
+const { parse } = require("../utils/number");
 
 let scrape = async (code) => {
-    try {
-        const { data } = await axios.get(`https://br.advfn.com/bolsa-de-valores/bovespa/${code}/cotacao`);
-        const $ = cheerio.load(data);
+  try {
+    const { data } = await axios.get(
+      `https://br.advfn.com/bolsa-de-valores/bovespa/${code}/cotacao`
+    );
+    const $ = cheerio.load(data);
 
-        let valueString = $('#quoteElementPiece1').text();
-        let value = parse(valueString);
+    let valueString = $("[data-asc-param=CUR_PRICE]").text();
+    let value = parse(valueString);
 
-        if (value) {
-            return value;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        logger.error(error);
-        return false;
+    if (value) {
+      return value;
+    } else {
+      return false;
     }
-}
+  } catch (error) {
+    logger.error(error);
+    return false;
+  }
+};
 
 module.exports = scrape;
